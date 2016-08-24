c4937ec601863934269a0a0d47c4bfa8628760fb
Tinet-jinhong <jinhong@ti-net.com.cn>
2016-08-24 11:19:00 +0800

    a

--- src/main/java/com/tinet/test/jh/Test.java
+++ src/main/java/com/tinet/test/jh/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Hello World a");
+		System.out.println("Hello World b");
 	}
 }
