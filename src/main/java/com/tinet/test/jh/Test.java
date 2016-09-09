297e76aaeef5a790598318a63def09e7108451fa
Tinet-jinhong <jinhong@ti-net.com.cn>
2016-09-09 15:37:20 +0800

    t

--- src/main/java/com/tinet/test/jh/Test.java
+++ src/main/java/com/tinet/test/jh/Test.java
@@ -3,13 +3,11 @@ package com.tinet.test.jh;
 public class Test {
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("aaa");
-		System.out.println("bbb");
 		System.out.println("Hello World");
 		System.out.println("Hello World a");
 		System.out.println("Hello World b");
 		System.out.println("Hello World c");
 		System.out.println("Hello World d");
+		System.out.println("##########");
 	}
 }
