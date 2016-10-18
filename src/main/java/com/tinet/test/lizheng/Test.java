d3771c00452916f747f8fdb6de27e3faef76dd5c
Tinet-lizheng <lizhengyeah@qq.com>
2016-10-18 09:57:24 +0800

    no message

--- src/main/java/com/tinet/test/lizheng/Test.java
+++ src/main/java/com/tinet/test/lizheng/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("2016-8-20 第一次分支提交");
 		System.out.println("2016-8-20 第二次分支提交");
 		System.out.println("2016-8-20 第三次分支提交");
+		System.out.println("改了这个文件");
 	}
 
 }
